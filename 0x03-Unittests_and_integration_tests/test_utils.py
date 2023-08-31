712a1bd912a7079f55c74b15784584a8c3196130
David Surumen <davidsurumen@gmail.com>
2023-08-31 22:18:22 +0300

    task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,7 +14,7 @@ class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
         ({"a": 1}, ["a"], 1),
         ({"a": {"b": 2}}, ["a"], {"b": 2}),
-        ({"a": {"b": 2}}, ["a", "b"], 2)
+        ({"a": {"b": 2}}, ["a", "b"], 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         """
@@ -34,3 +34,17 @@ class TestAccessNestedMap(unittest.TestCase):
               the expected value.
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ["a"], KeyError, "'a'"),
+        ({"a": 1}, ["a", "b"], KeyError, "'b'"),
+    ])
+    def test_access_nested_map_exception(self, nested_map,
+                                         path, expected_excep, expected_msg):
+        """
+        doc--
+        """
+        with self.assertRaises(expected_excep) as context:
+            access_nested_map(nested_map, path)
+
+        self.assertEqual(str(context.exception), expected_msg)
